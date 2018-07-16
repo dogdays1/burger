8b94d654cf76e2632ff80fe4499272c0ad6d1557
dogdays1 <scarrc@bellsouth.net>
2018-07-16 09:59:58 -0400

    homework

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+
+
+
+DROP DATABASE IF EXISTS burgers_DB;
+CREATE DATABASE burgers_DB;
+
+USE burgers_DB;
+
+CREATE TABLE burgers (
+  id INT NOT NULL AUTO_INCREMENT,
+  burger_name VARCHAR(30) NOT NULL,
+  devoured BOOLEAN,
+  PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Buffalo Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Black Bean Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Monster Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("BlackJack Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Hop-A-Long Burger", false);
